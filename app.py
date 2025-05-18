3ef51290dc6967a551f8bdcfaa44157027d8e517
szymon <szymon.marciniak0@icloud.com>
2025-05-18 22:59:57 +0200

    Creating directories

---
+++ app.py
@@ -0,0 +1,16 @@
+# This is a simple Flask application that visualise Dijkstra backpack problem.
+from flask import Flask, request, jsonify, render_template
+from algorithm.dijkstra import dijkstra, get_path, get_items
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(
+        host='localhost',
+        port=8080,
+        debug=True
+    )
